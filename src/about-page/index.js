d98339770b01c7677e23c7adc7863596fc130ec7
Escape <Escape>
2022-03-20 23:49:20 +0700

    responsive about page

--- src/about-page/index.js
+++ src/about-page/index.js
@@ -1,4 +1,3 @@
-
 import React, { useState, useEffect } from "react";
 import "./index.css";
 import Screenloading from "../loading_screen/index";
@@ -24,6 +23,7 @@ const Aboutpage = () => {
           <Navbarforall />
           <div className="aboutpage_main_container">
             <section className="aboutpage_first_section">
+              <div className="artist_profile_image2"></div>
               <div className="aboutpage_content">
                 <text>About the Artist</text>
                 <h1>
@@ -45,21 +45,27 @@ const Aboutpage = () => {
                 <p>
                   Solo Exhibitions<br></br>
                   2019《The Quiet Trip》Black Sugar Coffee / Hong Kong <br></br>
-                  2018《The 6th Floor Zoooo!》Dawning Crow Studio / Hong Kong <br></br>
-                  2018《Those Moving Darkness》Black Sugar Coffee / Hong Kong <br></br>
-                  2018《The Zoo, and the Black Dog》3rd Floor Studio / Taiwan <br></br>
+                  2018《The 6th Floor Zoooo!》Dawning Crow Studio / Hong Kong{" "}
+                  <br></br>
+                  2018《Those Moving Darkness》Black Sugar Coffee / Hong Kong{" "}
+                  <br></br>
+                  2018《The Zoo, and the Black Dog》3rd Floor Studio / Taiwan{" "}
+                  <br></br>
                   2017 《HUH HUH HUH HUH》Café+ KUBRICK / Hong Kong <br></br>
                   <br></br>
-                  Group Exhibitions <br></br> 
-                  2017 《Terrier Terrier》 JCCAC Visual Art Festival / Hong Kong <br></br>
-                  2017《Star Street Life is Art POP UP Festival》/ Kubrick ArtPick / Hong Kong <br></br>
+                  Group Exhibitions <br></br>
+                  2017 《Terrier Terrier》 JCCAC Visual Art Festival / Hong Kong{" "}
+                  <br></br>
+                  2017《Star Street Life is Art POP UP Festival》/ Kubrick
+                  ArtPick / Hong Kong <br></br>
                   2015 《Journey of Flavor》Red Barn / Taiwan <br></br>
                   2013《Car Park Show》England <br></br>
-                  2013 《Brighton Graduate Show》England 
+                  2013 《Brighton Graduate Show》England
                 </p>
               </div>
             </section>
             <section className="aboutpage_third_section">
+              <div className="shop_profile_image2"></div>
               <div className="aboutpage_content">
                 <text>About Dawning Crow</text>
                 <h1>
@@ -67,10 +73,9 @@ const Aboutpage = () => {
                 </h1>
                 <p>
                   創作者「黑山」的部落格和商店名稱<br></br>
-                  主要製作動物的藝術作品與商品。<br></br>
-
-                    A blog and shop name by Artist Kathy Lam.<br></br>
-                    Mainly making animal artworks and products. <br></br>
+                  主要製作動物的藝術作品與商品。<br></br>A blog and shop name by
+                  Artist Kathy Lam.<br></br>
+                  Mainly making animal artworks and products. <br></br>
                 </p>
               </div>
               <div className="shop_profile_image"></div>
